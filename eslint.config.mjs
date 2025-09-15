e1c99d596f9c515535ee37a44ee62b0714702f22
NELK9 <2262881585@qq.com>
2025-09-15 17:49:03 +0800

    eslint配置

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,11 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigPrettier from "eslint-config-prettier";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import prettierPlugin from "eslint-plugin-prettier";
+import unusedImports from "eslint-plugin-unused-imports";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -19,6 +24,54 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      prettier: prettierPlugin,
+      "unused-imports": unusedImports,
+    },
+    rules: {
+      ...eslintConfigPrettier.rules,
+      "no-console": "warn",
+      "@typescript-eslint/no-unused-vars": "off",
+      'unused-imports/no-unused-imports': 'warn',
+      'unused-imports/no-unused-vars': 'warn',
+      "@typescript-eslint/no-explicit-any": "warn",
+      "prettier/prettier": [
+        "error",
+        { singleQuote: true, semi: false, printWidth: 100, tabWidth: 2 },
+      ],
+      "import/order": [
+        "warn",
+        {
+          groups: [
+            "builtin", // Node.js 内置模块
+            "external", // npm 包
+            "internal", // 别名或绝对路径
+            ["parent", "sibling", "index"],
+            "object",
+            "type",
+          ],
+          pathGroups: [
+            {
+              pattern: "react*", // 匹配 react, react-dom 等
+              group: "external",
+              position: "before",
+            },
+            { pattern: "@/**", group: "internal", position: "after" },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+    }
   },
 ];
 
