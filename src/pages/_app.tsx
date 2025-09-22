5597d154303ed966a4b779c83e458853f60b89ea
NELK9 <2262881585@qq.com>
2025-09-22 13:41:24 +0800

    feat:配置toast和网页页签标题

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.scss'
 import '@/styles/shadcn.css'
 import { ReactElement, ReactNode } from 'react'
 import { NextPage } from 'next'
+import dynamic from 'next/dynamic'
 import Layout from '@/layouts'
 import type { AppProps } from 'next/app'
 
@@ -13,7 +14,16 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
 }
 
+const Toaster = dynamic(() => import('react-hot-toast').then((mod) => mod.Toaster), {
+  ssr: false,
+})
+
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => <Layout>{page}</Layout>)
-  return getLayout(<Component {...pageProps} />)
+  return (
+    <>
+      <Toaster position="top-center" containerStyle={{ top: 100 }} />
+      {getLayout(<Component {...pageProps} />)}
+    </>
+  )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" className={'light'}>
-      <Head />
+      <Head>
+        <title>印鉴通</title>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
